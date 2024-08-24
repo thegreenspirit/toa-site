8eab5084826f8ea4bb96bc1783447c7b645f5f55
The Green Spirit <tratriet89@gmail.com>
2024-08-24 23:38:18 +0700

    contact

---
+++ contact.php
@@ -0,0 +1,41 @@
+<?php
+$includepath = '_inc';
+$dataUrl = "http://localhost/data";
+$title = "Contact Us";
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://www.facebook.com/2008/fbml" xml:lang="en" lang="en">
+	<head>
+		<?php include $includepath . '/_headMeta.php' ?>
+		<?php include $includepath . '/_head.php' ?>
+	</head>
+
+	<body>
+		<div id="wrapper">
+			<div id="container">
+				<?php include $includepath . '/_header.php' ?>
+<div class="clear"></div>
+				
+				<div class="margintop_20px marginleft_5px marginright_5px">
+					<h1 class="tetris_game_header">Contact Us</h1>
+					<br/>
+					<h2>Email</h2>
+					<p>
+						PR Inquiries: <a href="mailto:press@tetrisonline.com?subject=TetrisOnline.com Referral | PR Inquiry">press@tetrisonline.com</a><br/>
+						Technical Support for Our Products: <a href="mailto:support@tetrisonline.com?subject=TetrisOnline.com Referral | Support">support@tetrisonline.com</a><br/>
+						Advertising Opportunities: <a href="mailto:sales@tetrisonline.com?subject=TetrisOnline.com Referral | Advertising Opportunity">sales@tetrisonline.com</a><br/>
+					<p>
+					
+					<h2>Mailing Address</h2>
+					<p>
+						Tetris Online Inc.<br/>
+						55 Merchant Street, Suite 2100<br/> <!-- Floor 17 -->
+						Honolulu, HI 96813
+					</p>
+				</div>
+				<?php include $includepath . '/_footer.php' ?>
+		</div>
+		</div>
+	</body>
+</html>
